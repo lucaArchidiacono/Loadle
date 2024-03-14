042e3a3018892d9db52628e9b94d240aae10fa6d
lucaArchidiacono <luca@swift-mail.com>
2024-03-14 17:32:08 +0100

    fixup! Add fetch for counting how many MediaAssetItem's are in MediaService

--- Packages/Models/Sources/Models/MediaService.swift
+++ Packages/Models/Sources/Models/MediaService.swift
@@ -84,6 +84,7 @@ public enum MediaService: String, Hashable, Identifiable, CaseIterable, Codable 
 				Spacer()
 				if let count {
 					Text("\(count)")
+						.fontWeight(.ultraLight)
 				}
 			}
 		} icon: {
