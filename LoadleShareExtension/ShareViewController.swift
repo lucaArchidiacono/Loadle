1f6e73e8665fe2d31c29d0112e4e07f784f2fadf
lucaArchidiacono <luca@swift-mail.com>
2024-03-27 16:14:25 +0100

    Make some progress with share extension

--- LoadleShareExtension/ShareViewController.swift
+++ LoadleShareExtension/ShareViewController.swift
@@ -6,11 +6,89 @@
 //
 
 import UIKit
+import Generator
+import SwiftUI
+
+struct ShareView: View {
+	var isLoading: Bool = true
+
+	var body: some View {
+		ZStack {
+			Rectangle()
+				.fill(.clear)
+				.overlay {
+					Rectangle()
+						.fill(Color(red: 46/255, green: 46/255, blue: 46/255))
+						.frame(width: 200, height: 200)
+						.cornerRadius(8)
+						.overlay {
+							VStack {
+								Assets.appIcon.swiftUIImage
+									.resizable()
+									.scaledToFit()
+									.frame(width: 50, height: 50)
+									.cornerRadius(8)
+
+								if isLoading {
+									ProgressView()
+								} else {
+									Text("\(L10n.done) ✅")
+								}
+							}
+						}
+				}
+		}
+	}
+}
 
 class ShareViewController: UIViewController {
-    private let viewModel = ShareViewModel()
+	private let viewModel = ShareViewModel()
+	private lazy var shareViewController: UIHostingController = {
+		let viewController = UIHostingController(rootView: ShareView())
+		return viewController
+	}()
 
-    override func viewDidLoad() {}
+    override func viewDidLoad() {
+		super.viewDidLoad()
 
-    private func setupView() {}
+		setupLayout()
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+
+		if let extensionContext, let item = extensionContext.inputItems.first as? NSExtensionItem {
+			Task { [unowned self] in
+				self.shareViewController.rootView = ShareView(isLoading: true)
+				
+				await self.viewModel.handleExtension(item)
+
+				self.shareViewController.rootView = ShareView(isLoading: false)
+
+				try? await Task.sleep(for: .seconds(3))
+				self.extensionContext?.completeRequest(returningItems: nil)
+			}
+		}
+	}
+}
+
+extension ShareViewController {
+	private func setupLayout() {
+		let swiftuiView = shareViewController.view!
+		swiftuiView.translatesAutoresizingMaskIntoConstraints = false
+		swiftuiView.isOpaque = false
+		swiftuiView.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+
+		addChild(shareViewController)
+		view.addSubview(swiftuiView)
+
+		NSLayoutConstraint.activate([
+			swiftuiView.topAnchor.constraint(equalTo: view.topAnchor),
+			swiftuiView.leftAnchor.constraint(equalTo: view.leftAnchor),
+			swiftuiView.rightAnchor.constraint(equalTo: view.rightAnchor),
+			swiftuiView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+		])
+
+		shareViewController.didMove(toParent: self)
+	}
 }
--- LoadleShareExtension/ShareViewModel.swift
+++ LoadleShareExtension/ShareViewModel.swift
@@ -6,7 +6,102 @@
 //
 
 import Foundation
+import Logger
+import Models
+import REST
+import Environments
+import UniformTypeIdentifiers
 
+@MainActor
 final class ShareViewModel {
-    init() {}
+	enum Error: Swift.Error, CustomStringConvertible {
+		case notReachable(URL)
+		case notSupported(URL)
+		case notAbleToLoad(registeredTypeIdentifiers: [String])
+		case noAttachamentsFound(title: NSAttributedString?, userInfo: [AnyHashable: Any]?)
+
+		var description: String {
+			let description = "\(type(of: self))."
+			switch self {
+			case let .notReachable(url):
+				return "\(description)notReachable: Was not able to reach the provided URL: \(url)"
+			case let .notSupported(url):
+				return "\(description)notSupported: The provided URL does not match a supported service: \(url)"
+			case let .notAbleToLoad(registeredTypeIdentifiers):
+				return "\(description)notAbleToLoad: The ItemProvider only supports the following registeredTypeIdentifiers: \(registeredTypeIdentifiers)"
+			case let .noAttachamentsFound(title, userInfo):
+				return "\(description)noAttachamentsFound: No attachements were found inside the NSExtensionItem. Title: \(title ?? NSAttributedString(string: "")), UserInfo: \(userInfo ?? [:])"
+			}
+		}
+	}
+
+	public init() {}
+
+	public func handleExtension(_ item: NSExtensionItem) async {
+		do {
+			guard let itemProvider = item.attachments?.first as? NSItemProvider else {
+				throw Error.noAttachamentsFound(title: item.attributedTitle, userInfo: item.userInfo)
+			}
+
+			guard let url = try await itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier) as? URL else {
+				throw Error.notAbleToLoad(registeredTypeIdentifiers: itemProvider.registeredTypeIdentifiers)
+			}
+
+			try await download(url: url)
+		} catch {
+			log(.error, error)
+		}
+	}
+
+	private func download(url: URL) async throws {
+		guard let mediaService = MediaService.allCases.first(where: { url.matchesRegex(pattern: $0.regex) }) else {
+			throw Error.notSupported(url)
+		}
+
+			let metadata = try await MetadataService.shared.fetch(using: url)
+
+			guard let url = metadata.url else {
+				throw Error.notReachable(url)
+			}
+
+			/// We need to first fetch the new url from the metada.
+			/// Reason for it has to do with the fact that URL's coming from iMessage or copying using third party apps (such as the in house Reddit App), provides an URL which is not recognised by the Cobalt API.
+			/// While using the URL and passing it to the MetaDataService, we should get the original URL (one which is visible by using the normal desktop browser).
+			let cobaltRequest = DataTransformer.Request.transform(
+				url: url,
+				mediaService: mediaService,
+				videoYoutubeCodec: UserPreferences.shared.videoYoutubeCodec,
+				videoDownloadQuality: UserPreferences.shared.videoDownloadQuality,
+				audioFormat: UserPreferences.shared.audioFormat,
+				audioOnly: false,
+				videoTiktokWatermarkDisabled: UserPreferences.shared.videoTiktokWatermarkDisabled,
+				audioTiktokFullAudio: UserPreferences.shared.audioTiktokFullAudio,
+				audioMute: UserPreferences.shared.audioMute,
+				audioYoutubeTrack: UserPreferences.shared.audioYoutubeTrack,
+				videoTwitterConvertGifsToGif: UserPreferences.shared.videoTwitterConvertGifsToGif,
+				videoVimeoDownloadType: UserPreferences.shared.videoVimeoDownloadType)
+			let request = REST.HTTPRequest(host: "co.wuk.sh", path: "/api/json", method: .post, body: REST.JSONBody(cobaltRequest))
+
+			let response = try await REST.Loader.shared.load(using: request)
+			let cobaltResponse: POSTCobaltResponse = try response.decode()
+
+			// If contains picker, then download everything
+			if let streamURL = cobaltResponse.url {
+				await DownloadService.shared.download(using: url, streamURL: streamURL, mediaService: mediaService, metadata: metadata)
+			} else {
+				await withTaskGroup(of: Void.self) { group in
+					if let audioStream = cobaltResponse.audio {
+						group.addTask {
+							await DownloadService.shared.download(using: url, streamURL: audioStream, mediaService: mediaService, metadata: metadata)
+						}
+					}
+
+					for picker in cobaltResponse.picker {
+						group.addTask {
+							await DownloadService.shared.download(using: url, streamURL: picker.url, mediaService: mediaService, metadata: metadata)
+						}
+					}
+				}
+			}
+	}
 }
