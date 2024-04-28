f81b52b10fea1006d4dca767b9e664badc999361
lucaArchidiacono <luca@swift-mail.com>
2024-04-28 13:00:36 +0200

    fixup! Initial transformation approach for reddit

--- Packages/Models/Sources/Models/Transformers/RequestTransformer.swift
+++ Packages/Models/Sources/Models/Transformers/RequestTransformer.swift
@@ -65,48 +65,7 @@ public extension DataTransformer.Request {
 		case .youtube: return url
 		case .instagram: return url
 		case .twitter: return url
-		case .reddit:
-			return url
-			// Split the path components
-			let pathComponents = url.pathComponents
-
-			// Find the index of "r" in the path
-			guard let rIndex = pathComponents.firstIndex(of: "r") else {
-				return url
-			}
-
-			// Ensure there's a subreddit after "r"
-			guard rIndex + 1 < pathComponents.count else {
-				return url
-			}
-
-			// Get the subreddit
-			let subreddit = pathComponents[rIndex + 1]
-
-			// Find the index of "comments" in the path
-			guard let commentsIndex = pathComponents.firstIndex(of: "comments") else {
-				return url
-			}
-
-			// Ensure there's a post ID after "comments"
-			guard commentsIndex + 1 < pathComponents.count else {
-				return url
-			}
-
-			// Get the post ID
-			let postID = pathComponents[commentsIndex + 1]
-
-			// Construct the goal URL
-			var components = URLComponents()
-			components.scheme = "https"
-			components.host = url.host
-			components.path = "/r/\(subreddit)/comments/\(postID)"
-
-			guard let transformedURL = components.url else {
-				return url
-			}
-
-			return transformedURL
+		case .reddit: return url
 		case .twitch: return url
 		case .pinterest: return url
 		case .bilibili: return url
